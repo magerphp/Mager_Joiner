5bba54806a2917fbd865499c59c8a120f6282dd6
Rob Simmons <rob.v.simmons@gmail.com>
2017-10-01 00:02:23 -0400

    Commits empty module

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Mager_Joiner',
+        __DIR__
+    );
